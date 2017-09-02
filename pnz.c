0dd2b82bf2dabf08a8a5d3bc01775e4a72eb72a7
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-09-02 18:42:39 +0530

    Create pnz.c

---
+++ pnz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int t;
+printf("Enter a number: ");
+scanf("%d",&number);
+if(number<=0)
+{
+if(mumber ==0)
+printf(" you entered 0 ");
+else
+printf(" you entered negative ");
+}
+else
+printf(" you entered positive ");
+return 0;
+}
