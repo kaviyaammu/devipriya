a2fe4dd046fce10d2f661332abcf06cc749202d6
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-09-03 12:51:17 +0530

    Create Pnzero.c

---
+++ Pnzero.c
@@ -0,0 +1,18 @@
+# devipriya
+#include<stdio.h>
+int main()
+{
+int t;
+printf("Enter the number: ");
+scanf("%d",&number);
+if(number<=0)
+{
+if(number==0)
+printf("you entered zero");
+else
+printf("you entered negative number");
+}
+else
+printf("you entered positive number");
+return 0;
+}
