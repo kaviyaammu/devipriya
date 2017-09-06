b0b672725e2af255a4c835238d7b4aef12aca4b9
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-09-06 21:46:26 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,15 @@
+# devipriya
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the numbers: ");
+scanf("%d,%d,%d",&a,&b,&c);
+if(a>=b&&a>=c)
+printf("%d is the largest number",a);
+elseif(b>=a&&b>=c)
+printf({"%d is the largest number",b);
+else
+printf("%d is the largest number",c);
+return 0;
+}
