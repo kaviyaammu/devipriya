e5db22325cbb0814a8220405bb1394519e2109af
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-09-06 20:05:09 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,13 @@
+# devipriya
+#include<stdio.h>
+int main()
+{
+char c;
+printf("Enter the character: ");
+scanf("%c",&c);
+if((c>='a'&&c<='z')||(C>='A'&&c<='Z'))
+printf("%c is alphabet. ",c);
+else
+printf("%c is not an alphabet. ",c);
+return 0;
+}
