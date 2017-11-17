0071a371413c460da87644b300dca769a9dce100
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-11-17 11:01:04 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,15 @@
+# devipriya
+#include<stdio.h>
+int main()
+{
+long long n;
+int count=0;
+printf("Enter the integers");
+scanf("lld",&n);
+while(n!=0)
+{
+n/=0;
+++count;
+}
+printf("Enter the digits= %d",count);
+}
