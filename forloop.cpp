78595e34e4d70729cb3b74f5c035c9ec21809ad8
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-11-17 10:51:19 +0530

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,13 @@
+# devipriya
+#include<iosteam.h>
+void main()
+{
+int n,sum=0;
+cout << "Enter the positive integers";
+cin >> n;
+for(int i=0;i<=n;++i)
+sum+=i;
+}
+cout << "sum="<<sum;
+return 0;
+}
