27b4050c535513e74110995369a10d13ccb55638
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-11-17 10:36:14 +0530

    Create forloop.m

---
+++ forloop.m
@@ -0,0 +1,14 @@
+# devipriya
+#include<iostream.h>
+void main()
+{
+int n,sum=0;
+cout << "enter the positive integers" ;
+cin >> n;
+for( int i=0;i<=n;++i){
+sum+=i;
+}
+cout << "sum="<<sum;
+return 0:
+}
+
